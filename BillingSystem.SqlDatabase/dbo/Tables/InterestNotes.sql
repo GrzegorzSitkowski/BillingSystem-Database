6206b8335a984562a8d44f47c1d90287ea628f8b
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-09-29 14:44:46 +0200

    Created new table - InterestNotes

---
+++ BillingSystem.SqlDatabase/dbo/Tables/InterestNotes.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[InterestNotes]
+(
+	[Id] INT IDENTITY(1,1) NOT NULL,
+	[InvoiceId] INT NOT NULL,
+	[CustomerId] INT NOT NULL,
+	[Amount] FLOAT NOT NULL,
+	[DocumentNumber] Varchar(50),
+	[InvoiceNumber] Varchar(50),
+	[DueDate] DATETIMEOFFSET NOT NULL,
+	[CreateDate] DATETIMEOFFSET NOT NULL,
+	[CreatedBy] INT NOT NULL,
+	CONSTRAINT [PK_InterestNotes] PRIMARY KEY CLUSTERED ([Id] ASC),
+	CONSTRAINT [FK_InterestNotes_Customers] FOREIGN KEY ([CustomerId]) REFERENCES [dbo].[Customers] ([Id]) ON DELETE CASCADE,
+)
