9d70cc22df2f278ca61dbfdc09929252c845aeb2
Grzegorz Sitkowski <sitkowskigrzegorz8@gmail.com>
2024-11-01 15:15:44 +0100

    Created table - Payments

---
+++ BillingSystem.SqlDatabase/dbo/Tables/Payment.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Payments]
+(
+	[Id] INT IDENTITY(1,1) NOT NULL,
+	[CustomerId] INT NOT NULL,
+	[DocumentId] INT NOT NULL,
+	[DocumentNumber] Varchar(50),
+	[Amount] FLOAT NOT NULL,
+	[CreateDate] DATETIMEOFFSET NOT NULL,
+	CONSTRAINT [Pk_Payments] PRIMARY KEY CLUSTERED ([Id] ASC),
+	CONSTRAINT [FK_Payments_Customers] FOREIGN KEY ([CustomerId]) REFERENCES [dbo].[Customers] ([Id]) ON DELETE CASCADE,
+	CONSTRAINT [FK_Payments_Invoices] FOREIGN KEY ([DocumentId]) REFERENCES [dbo].[Invoices] ([Id]),
+)
